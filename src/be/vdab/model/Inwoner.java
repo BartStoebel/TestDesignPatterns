27f234be069f9cb607a726604de1a2cbf78736b2
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-03-23 16:26:45 +0100

    Eerste classes gemaakt volgens UML model

---
+++ src/be/vdab/model/Inwoner.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.vdab.model;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class Inwoner {
+    private String naam;
+
+    public Inwoner(String naam) {
+        this.naam = naam;
+    }
+    
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    
+    
+}
---
+++ src/be/vdab/model/Vogel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.vdab.model;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class Vogel extends Inwoner implements Observer{
+
+    public Vogel(String naam) {
+        super(naam);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
